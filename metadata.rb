6b78aacc4df10b0553bd25792c304386153fd392
nat <nat.lownes@gmail.com>
2012-02-07 15:35:34 -0500

    deps fix

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ license          "Apache 2.0"
 description      "Installs/Configures kvm-cookbook"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
-depends          "apt", "ntp"
+depends          "apt"
