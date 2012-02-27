4611012ca96784075576adf611e755f13e8c53bf
nat <nat.lownes@gmail.com>
2012-02-26 21:50:55 -0500

    updating metadata, releasing 0.0.1

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,14 @@
-maintainer       "Fort Hill Company"
-maintainer_email "dev@forthillcompany.com"
+maintainer       "Nat Lownes"
+maintainer_email "nat.lownes@gmail.com"
+name             "kvm"
 license          "Apache 2.0"
 description      "Installs/Configures kvm-cookbook"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 depends          "apt"
+
+distros = %w(debian ubuntu)
+
+distros.each do |dist|
+  supports dist
+end
